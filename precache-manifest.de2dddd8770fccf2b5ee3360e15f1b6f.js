a8761714b3ef038704102e3abc52a6df87603b69
ImWonYong <ssassb123@daum.net>
2020-04-09 00:15:54 +0900

    Updates

---
+++ precache-manifest.de2dddd8770fccf2b5ee3360e15f1b6f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f3f69201bc71f5a113b3a19bb296014",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "b11d65533c0d4b0e73cb",
+    "url": "/movie_app/static/css/main.5e1d0d26.chunk.css"
+  },
+  {
+    "revision": "42901d470a654707de4f",
+    "url": "/movie_app/static/js/2.54999406.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app/static/js/2.54999406.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b11d65533c0d4b0e73cb",
+    "url": "/movie_app/static/js/main.a51a4f1c.chunk.js"
+  },
+  {
+    "revision": "77df4e0b724824eca1cd",
+    "url": "/movie_app/static/js/runtime-main.0b3e8895.js"
+  }
+]);
