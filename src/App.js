1a5be744f9d82e7aae3fb71b676ca9c1a200a80d
ImWonYong <ssassb123@daum.net>
2020-04-08 23:33:39 +0900

    movie app 꾸미기

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import PropTypes from "prop-types";
 import axios from "axios";
 import Movie from "./Movie";
+import "./App.css";
 
 class App extends React.Component {
   state = {
@@ -27,12 +28,15 @@ class App extends React.Component {
   }
   render() {
     const { isLoading, movies } = this.state;
-    console.log(movies);
     return (
-      <div>
-        {isLoading
-          ? "Loading..."
-          : movies.map(movie => (
+      <section className="container">
+        {isLoading ? (
+          <div className="loader">
+            <span className="loader__text">Loading...</span>
+          </div>
+        ) : (
+          <div className="movies">
+            {movies.map(movie => (
               <Movie
                 key={movie.id}
                 id={movie.id}
@@ -40,9 +44,12 @@ class App extends React.Component {
                 title={movie.title}
                 summary={movie.summary}
                 poster={movie.medium_cover_image}
+                genres={movie.genres}
               />
             ))}
-      </div>
+          </div>
+        )}
+      </section>
     );
   }
 }
